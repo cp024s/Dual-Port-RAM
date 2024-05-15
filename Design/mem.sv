20a853887805c3fcb7ce51cfc83a87a0def37df7
Chandra Prakash <62211521+cp024s@users.noreply.github.com>
2024-05-15 13:34:26 +0530

    Memory Module

--- Design/mem.sv
+++ Design/mem.sv
@@ -1,53 +1,38 @@
-// RTL for 1K RAM
+module dual_mem (
+    input clk,         // RAM Clock
+    input mem_en,      // Memory Enable
+    input op_en,       // Operation Enable
+    input [63:0] data_in,       // Data Input
+    input [9:0] rd_address,     // Read Address
+    input [9:0] wr_address,     // Write Address
+    input read,        // Read Control
+    input write,       // Write Control
+    output reg [63:0] data_out   // Data Output
+);
 
-module dual_mem ( clk,
-                  mem_en,
-                  op_en,
-                                data_in,
-                                rd_address,
-                                wr_address,
-                                read,
-                                write,
-                                data_out) ;
+    parameter RAM_WIDTH = 64;
+    parameter RAM_DEPTH = 1024;
+    parameter ADDR_SIZE = 10;
 
-        parameter RAM_WIDTH=64,
-                          RAM_DEPTH=1024,
-                          ADDR_SIZE=10;
+    // Memory
+    reg [63:0] memory [0:RAM_DEPTH-1];
 
+    always @ (posedge clk) begin
+        if (mem_en) begin
+            if (write) begin
+                memory[wr_address] <= data_in;
+            end
+        end
+    end
 
-        input clk;                              // RAM Clock
-        input [RAM_WIDTH-1 : 0] data_in;        // Data Input
-        input [ADDR_SIZE-1 : 0] rd_address;     // Read Address
-        input [ADDR_SIZE-1 : 0] wr_address;     // Write Address
-        input read;                             // Read Control
-        input write;                            // Write Control
-        input mem_en;                           //memory enable
-        input op_en;
-
-        output [RAM_WIDTH-1 : 0] data_out;      // Data Output
-
-
-        reg [RAM_WIDTH-1 : 0] data_out;
-
-        // Memory
-        reg [RAM_WIDTH-1 : 0] memory [RAM_DEPTH-1 : 0];
-
-        //Read
-        always @ (posedge clk)
-        if (mem_en)
-                begin
-                        if(write)
-                        memory[wr_address] <= data_in;
-                end
-
-        //Write
-        always @ (posedge clk)
-        if (op_en)
-                begin
-                        if(read)
-                        data_out <= memory[rd_address];
-                end
-        else
-                data_out <=64'bz;
+    always @ (posedge clk) begin
+        if (op_en) begin
+            if (read) begin
+                data_out <= memory[rd_address];
+            end
+        end else begin
+            data_out <= 64'bZ; // High-impedance output when operation is disabled
+        end
+    end
 
 endmodule
